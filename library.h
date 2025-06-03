2b93e19ac9a6c15ce46938d843edf8c62fc50fab
rodrigoo-r <2nw53j32643y@gmail.com>
2025-06-03 12:47:08 -0500

    Add ANSI color definitions to library.h

--- library.h
+++ library.h
@@ -11,6 +11,30 @@
 #ifndef FLUENT_LIBC_ANSI_LIBRARY_H
 #define FLUENT_LIBC_ANSI_LIBRARY_H
 
-void hello(void);
+// ============= FLUENT LIB C =============
+//
+#    define ANSI_RESET "\033[0m"
+#    define ANSI_BLACK "\033[30m"
+#    define ANSI_RED "\033[31m"
+#    define ANSI_GREEN "\033[32m"
+#    define ANSI_YELLOW "\033[33m"
+#    define ANSI_BLUE "\033[34m"
+#    define ANSI_PURPLE "\033[35m"
+#    define ANSI_CYAN "\033[36m"
+#    define ANSI_WHITE "\033[37m"
+#    define ANSI_BRIGHT_BLACK "\033[90m"
+#    define ANSI_BRIGHT_RED "\033[91m"
+#    define ANSI_BRIGHT_GREEN "\033[92m"
+#    define ANSI_BRIGHT_YELLOW "\033[93m"
+#    define ANSI_BRIGHT_BLUE "\033[94m"
+#    define ANSI_BRIGHT_PURPLE "\033[95m"
+#    define ANSI_BRIGHT_CYAN "\033[96m"
+#    define ANSI_BRIGHT_WHITE "\033[97m"
+#    define ANSI_BOLD "\033[1m"
+#    define ANSI_BOLD_BRIGHT_BLUE "\033[1;94m"
+#    define ANSI_BOLD_BRIGHT_GREEN "\033[1;92m"
+#    define ANSI_BOLD_BRIGHT_RED "\033[1;91m"
+#    define ANSI_BOLD_BRIGHT_YELLOW "\033[1;93m"
+#    define ANSI_BOLD_BRIGHT_PURPLE "\033[1;95m"
 
 #endif //FLUENT_LIBC_ANSI_LIBRARY_H
